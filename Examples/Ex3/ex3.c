a7df512ad85fe29aaf7997b133aa50a208f41b2d
Hong Jupyo <hshh0113@gmail.com>
2022-03-09 16:35:12 +0900

    ex3

---
+++ Examples/Ex3/ex3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print1(int*, int);
+
+int main()
+{
+    int one[] = {0, 1, 2, 3, 4};
+    print1(one, 5);
+}
+
+void print1(int *ptr, int rows)
+{
+    /* print out an one-dimensional array using a pointer */
+    int i;
+    printf("Address        Contents\n");
+    for(i=0; i<rows; i++)
+    {
+        printf("%8p%5d\n", ptr+i, *(ptr+i));
+    }
+    printf("\n");
+}
---
+++ Examples/Ex3/readme.txt
@@ -0,0 +1,1 @@
+- 'print1' is a function that prints out both the address of the i-th element and the value fount at this address.
