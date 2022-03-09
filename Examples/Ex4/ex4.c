410171401b43d1f6e8fd828d0000fc7cfe233bc0
Hong Jupyo <hshh0113@gmail.com>
2022-03-09 16:40:00 +0900

    ex4

---
+++ Examples/Ex4/ex4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, n, m, *data;
+    
+    printf("How many integers do you want to generate? ");
+    scanf("%d", &n);
+    
+    data = malloc(n * sizeof(int));
+
+    for(i=0; i<n; i++)
+    {
+        data[i] = rand();
+    }
+
+    printf("How many integers do you wnat to generate additionally? ");
+    scanf("%d", &m);
+
+    data = realloc(data, (n+m) * sizeof(int));
+    
+    for(i; i<n+m; i++)
+    {
+        data[i] = rand();
+    }
+
+    for(i=0; i<n+m; i++)
+    {
+        printf("%3d: %8d\n", i+1, data[i]);
+    }
+
+    free(data);
+}
