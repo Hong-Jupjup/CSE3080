5b0c9fbc06b41f3cd75ad4b177f4523b5ae05d73
Hong Jupyo <hshh0113@gmail.com>
2022-03-09 13:57:50 +0900

    test

--- Examples/Ex1/ex1.c
+++ Examples/Ex1/ex1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 1001
+#define MAX 1002
 
 int main()
 {
@@ -41,6 +41,6 @@ int main()
             printf("%d ", sorted[i]);
         }
         printf("\n");
-        
+
     }
 }
