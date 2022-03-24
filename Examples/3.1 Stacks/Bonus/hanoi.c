fd2a55d0136755837d7cd6bcf0360722a7b4c4df
Hong Jupyo <hshh0113@gmail.com>
2022-03-24 21:03:52 +0900

    hanoi

---
+++ Examples/3.1 Stacks/Bonus/hanoi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void hanoi_tower(int, char, char, char);
+
+int main()
+{
+    int floor = 3;
+
+    hanoi_tower(floor, 'A', 'B', 'C');
+}
+
+void hanoi_tower(int n, char from, char tmp, char to)
+{
+    if(n == 1) {
+        printf("No.1 disk moves from %c to %c.\n", from, to);
+    }
+    else {
+        hanoi_tower(n-1, from ,to, tmp);
+        printf("No.%d disk moves from %c to %c.\n", n, from, to);
+        hanoi_tower(n-1, tmp, from, to);
+    }
+}
---
+++ Examples/3.1 Stacks/Bonus/readme.txt
@@ -0,0 +1,1 @@
+- Make a 'Hanoi Tower' program.
