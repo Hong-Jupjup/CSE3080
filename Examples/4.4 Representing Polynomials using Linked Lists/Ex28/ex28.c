b2c6f79272488fc9a7144ded74ff26d58bd0a5d4
Hong Jupyo <hshh0113@gmail.com>
2022-04-07 13:47:47 +0900

    Ex28

---
+++ Examples/4.4 Representing Polynomials using Linked Lists/Ex28/ex28.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list_node {
+    char data;
+    struct list_node *link;
+};
+typedef struct list_node* list_pointer;
+
+list_pointer invert(list_pointer);
+void print_list(list_pointer);
+
+int main() {
+
+    /* make four list nodes and connect them to make a list */
+    list_pointer head, a, b, c, d, inverted;
+    a = (list_pointer)malloc(sizeof(struct list_node));
+    b = (list_pointer)malloc(sizeof(struct list_node));
+    c = (list_pointer)malloc(sizeof(struct list_node));
+    d = (list_pointer)malloc(sizeof(struct list_node));
+    a->data = 'A';
+    b->data = 'B';
+    c->data = 'C';
+    d->data = 'D';
+    a->link = b;
+    b->link = c;
+    c->link = d;
+    d->link = NULL;
+    head = a;
+
+    print_list(head);
+    inverted = invert(head);
+    print_list(inverted);
+}
+
+list_pointer invert(list_pointer lead)
+{
+    /* invert the list pointed by lead */
+    list_pointer middle, trail;
+    middle = NULL;
+    while(lead) {
+        trail = middle;
+        middle = lead;
+        lead = lead->link;
+        middle->link = trail;
+    }
+    return middle;
+}
+
+void print_list(list_pointer lead)
+{
+    list_pointer tmp = lead;
+    while(tmp) {
+        printf("%c ", tmp->data);
+        tmp = tmp->link;
+    }
+    printf("\n");
+}
---
+++ Examples/4.4 Representing Polynomials using Linked Lists/Ex28/readme.txt
@@ -0,0 +1,1 @@
+- Operations for Chains (inverting).
