2ef3daa135c6d03ee49a8c52553d297c69dcc1b2
Hong Jupyo <hshh0113@gmail.com>
2022-04-24 11:37:09 +0900

    Ex1

--- Examples/1. Warm Up/Ex1/ex1.c
+++ Examples/1. Warm Up/Ex1/ex1.c
@@ -16,9 +16,9 @@ int main()
         unsorted[index] = input;
         sorted[index++] = input;
 
-        for(i=0; i<index-1; i++){
-            for(j=0; j<index-1-i; j++){
-                if(sorted[j]>sorted[j+1]){
+        for(i=0; i<index-1; i++) {
+            for(j=0; j<index-1-i; j++) {
+                if(sorted[j]>sorted[j+1]) {
                     tmp = sorted[j];
                     sorted[j] = sorted[j+1];
                     sorted[j+1] = tmp;
@@ -27,11 +27,11 @@ int main()
         }
 
         printf("unsorted: ");
-        for (i=0; i<index; i++){
+        for (i=0; i<index; i++) {
             printf("%d ", unsorted[i]);
         }
         printf("\nsorted: ");
-        for(i=0; i<index; i++){
+        for(i=0; i<index; i++) {
             printf("%d ", sorted[i]);
         }
         printf("\n");
