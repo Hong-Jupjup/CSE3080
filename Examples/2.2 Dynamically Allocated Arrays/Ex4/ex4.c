a089ef4a9fca225a3c37ae33c9b16e4633c1c403
Hong Jupyo <hshh0113@gmail.com>
2022-04-24 11:48:57 +0900

    Ex4

--- Examples/2.2 Dynamically Allocated Arrays/Ex4/ex4.c
+++ Examples/2.2 Dynamically Allocated Arrays/Ex4/ex4.c
@@ -10,23 +10,20 @@ int main()
     
     data = malloc(n * sizeof(int));
 
-    for(i=0; i<n; i++)
-    {
+    for(i=0; i<n; i++) {
         data[i] = rand();
     }
 
-    printf("How many integers do you wnat to generate additionally? ");
+    printf("How many integers do you want to generate additionally? ");
     scanf("%d", &m);
 
     data = realloc(data, (n+m) * sizeof(int));
     
-    for(i; i<n+m; i++)
-    {
+    for(i; i<n+m; i++) {
         data[i] = rand();
     }
 
-    for(i=0; i<n+m; i++)
-    {
+    for(i=0; i<n+m; i++) {
         printf("%3d: %8d\n", i+1, data[i]);
     }
 
