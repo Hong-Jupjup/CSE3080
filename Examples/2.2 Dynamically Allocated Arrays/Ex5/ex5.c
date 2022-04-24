35b1148c2dd01f50804e1f3025c18212a7701332
Hong Jupyo <hshh0113@gmail.com>
2022-04-24 11:52:53 +0900

    Ex5

--- Examples/2.2 Dynamically Allocated Arrays/Ex5/ex5.c
+++ Examples/2.2 Dynamically Allocated Arrays/Ex5/ex5.c
@@ -10,18 +10,15 @@ int main()
 
     matrix = make2dArray(5, 5);
 
-    for(i=0; i<5; i++)
-    {
-        for(j=0; j<5; j++)
-        {
+    for(i=0; i<5; i++) {
+        for(j=0; j<5; j++) {
             matrix[i][j] = i*5 + j + 1;
             printf("%p: %2d| ", &matrix[i][j], matrix[i][j]);
         }
         printf("\n");
     }
 
-    for(i=0; i<5; i++)
-    {
+    for(i=0; i<5; i++) {
         free(matrix[i]);
     }
     free(matrix);
@@ -35,8 +32,7 @@ int** make2dArray(int rows, int cols)
     x = malloc(rows * sizeof(*x));
 
     /* allocate memory for each row */
-    for(i=0; i<rows; i++)
-    {
+    for(i=0; i<rows; i++) {
         x[i] = malloc(cols * sizeof(**x));
     }
 
