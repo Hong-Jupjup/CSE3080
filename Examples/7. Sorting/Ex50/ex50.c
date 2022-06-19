c5f11170038b544c58fc00ac98f7bef08a2c8f1d
Hong Jupyo <hshh0113@gmail.com>
2022-06-19 21:17:42 +0900

    Ex50

--- Examples/7. Sorting/Ex50/ex50.c
+++ Examples/7. Sorting/Ex50/ex50.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_NUMBERS 100
+#define MAX_NUMBERS 10
 #define MAX_VALUE 100
 
 void init_list(int*, int);
@@ -13,11 +13,11 @@ void quick_sort(int*, int, int);
 int main()
 {
     int i;
-    int list[MAX_NUMBERS];
+    int list[MAX_NUMBERS] = {50, 91, 19, 74, 2, 4, 52, 17, 80, 44};
 
     srand(time(NULL));
 
-    init_list(list, 10);
+//    init_list(list, 10);
     print_list(list, 10);
     quick_sort(list, 0, 9);
     print_list(list, 10);
@@ -54,6 +54,11 @@ int partition(int *list, int left, int right)
     tmp = list[right];
     list[right] = list[pivot];
     list[pivot] = tmp;
+
+    for(i=0; i<MAX_NUMBERS; i++) {
+        printf("%d\t", list[i]);
+    }
+    printf("\n%d\n", pivot);
     return pivot;
 }
 
@@ -63,6 +68,7 @@ void quick_sort(int *list, int left, int right)
 
     if(right > left) {
         pivot = partition(list, left, right);
+        
 
         quick_sort(list, left, pivot-1);
         quick_sort(list, pivot+1, right);
